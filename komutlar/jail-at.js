8947e6e394196bc9f618701741f7839402b72e69
Glitch (sunucubotu) <none>
2020-10-16 12:30:46 +0000

    🛥🍄 Checkpoint ./komutlar/jail-at.js:23205691/303 ./ayarlar.json:23205691/110

--- komutlar/jail-at.js
+++ komutlar/jail-at.js
@@ -3,10 +3,10 @@ const ayarlar = require('../ayarlar.json')
 exports.run = function(client, message, args) {
   let abone = message.mentions.members.first()
   let rol = ayarlar.jailROL
-  if(!message.member.hasPermission('ADMINISTRATOR')) return
-   if(!abone) return message.channel.send('Vip vereceğin kişiyi etiketlemelisin.')
+  if(!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send('Yetkin yok')
+   if(!abone) return message.channel.send('Jaile atacağın kişiyi etiketlemelisin.')
   var role = message.guild.roles.find(role => role.id === rol); 
-  abone.setRole(role);
+  abone.setRoles(['766634561583972373']);
   let embed = new Discord.RichEmbed()
   .setTitle(` <a:jke:751558669585612830> • __\` Kullanıcı Başarıyla Jaile Atıldı  \`__   `)
   .setDescription(`<a:jke:751558669585612830> • __**\` Yetkili \`**__ ${message.author}`)
